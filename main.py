83caf062125d6e1a2d9b8455e792f8c9a35ccf04
Alfred <alfredmaple@gmail.com>
2020-11-25 15:12:22 -0500

    Reorganized .py files into chapters

--- main.py
+++ main.py
@@ -1,39 +1,82 @@
-# Write a program to find the node at which the intersection of two singly linked lists begins.
 from typing import List
-
-def paritybitBrute(integer_64: int) -> int:
-    parity_count = 0
-    while integer_64:
-        parity_count ^= integer_64 & 1
-        integer_64 >>= 1
-    return parity_count
+from random import randint
 
 
-def paritybit2(integer_64: int) -> int:
-    dict_16Bit = {}
-    parity_count = 0
-    while integer_64:
-        lowest_16 = integer_64 & 0xffff
-        integer_64 >>= 16
-        if lowest_16 in dict_16Bit:
-            parity_count ^= dict_16Bit[lowest_16]
-        else:
-            low_parity = 0
-            while lowest_16:
-                low_parity ^= 1
-                lowest_16 &= lowest_16 - 1
-            dict_16Bit[lowest_16] = low_parity
-            parity_count ^=low_parity
-    return parity_count
+#Given an int, return a list of all primes less than int
+#Hint, remove all multiples of prime
+def primeListBrute(a:int)->List[int]:
+    if a<=2:
+        return a
 
-# Press the green button in the gutter to run the script.
+    pa = [2]
+    pflag = 1
+    for x in range(3,a+1):
+        for prime in pa:
+            if x%prime == 0:
+                pflag = 0
+                break
+        if pflag:
+            pa.append(x)
+        pflag = 1
+    return pa
+
+#Here we remove all multiples of the prime before going to the next number
+#O(n^(3/2)) time because it is dictated by the multiples of prime p (O(n/2+n/3+n/5+...))
+def primeListSmart1(a:int)->List[int]:
+    if a<=2:
+        return a
+    #This list dictates if the number (by idx) is a prime.
+    prime_list = [False,False] + [True] * (a-1)
+    pa = []
+    for x in range(2,a+1):
+        #If number is a prime
+        if prime_list[x]:
+            pa.append(x)
+
+            #Remove all multiples of that prime
+            for mp in range (x,a+1,x):
+                prime_list[mp] = False
+    return pa
+
+#NOTe We can improve the previous method by realizing that multiples kp (k<p, p prime)
+# were removed by the property of the method (since k<p). Allowing us to remove multiples of p
+#  FROM p^2 instead of p
+#Storage can also be reduced by ignoring even numbers
+
+def primeListSmart2(a:int)->List[int]:
+    if a<=2:
+        return a
+    #Size of our prime_list have been halved (with even numbers, 0, 1) removed
+    size = (a-3)//2 + 1
+    #prime_list[i] represents integer 2*i + 3
+    prime_list = [True] * size
+    pa = [2]
+    for i in range(size):
+        #If number is a prime
+        if prime_list[i]:
+            p = 2*i + 3
+            pa.append(p)
+            #if p = 2*i+3, p^2 =  4i^2+12i+9. Since prime_list[i] = 2i+3
+            #to get the right index x s.t. prime_list[x] = 2x+3 = p^2,
+            # x = 2i^2 + 6i + 3
+            #Remove all multiples of p starting from p^2
+            for mp in range (2*i**2 + 6*i + 3,size,p):
+                prime_list[mp] = False
+    return pa
 if __name__ == '__main__':
-    for number in range(10**5):
-        deq_res = paritybit2(number)
-        trivial_res = paritybitBrute(number)
-        assert deq_res == trivial_res, \
-            'deque = {}, trivial = {}, number = {}'.format(
-                deq_res,
-                trivial_res,
-                number
-            )
+    num = 19
+    print(primeListBrute(num))
+    print(primeListSmart1(num))
+    print(primeListSmart2(num))
+
+    # for tests in range(10000):
+    #     array = []
+    #     for i in range(20):
+    #         array.append(randint(1, 100))
+    #     my_res = stockSellTwiceSmarter(array)
+    #     trivial_res = stockSellTwiceTrivial(array)
+    #     assert my_res == trivial_res, \
+    #         'mine = {}, trivial = {}, array = {}'.format(
+    #             deq_res,
+    #             trivial_res,
+    #             array            )
